3a3800edc1ed2d14cca7dda49abc98f445da496f
Hannah Bolte <hbolte@Hannahs-MBP.PK5001Z>
2021-03-18 19:45:01 -0400

    updated file... doesn't change anything

--- magnesium.R
+++ magnesium.R
@@ -1,24 +1,24 @@
 library(readr)
 library(dplyr)
 library(tidyr)
-iMg = read_csv("iMg_EXTRACTION_Resultsfinal_Ansu.csv")
-iMg = iMg[,c(1, 4, 7, 9, 10, 11, 12)] # 261 rows
+iMg = read_csv("iMgReview.csv")
+iMg = iMg[,c(1, 4, 7, 9, 10, 12)] # 261 rows
 iMg = iMg[!is.na(iMg$grp1_base_mean),] # remove 18 rows without means
 iMg = iMg[!is.na(iMg$grp1_base_n),] # remove 10 rows without sample sizes
 iMg = unite(iMg, group, 2:3, remove = T)
 
 # replace missing sd with sd calculated from se and n
-missing.sd = is.na(iMg$grp1_base_sd)
-iMg$grp1_base_sd[missing.sd] = with(iMg, grp1_base_se[missing.sd] * 
-                                      sqrt(grp1_base_n[missing.sd]))
-iMg = iMg[!is.na(iMg$grp1_base_sd),] # remove 22 rows without sd
+#missing.sd = is.na(iMg$grp1_base_sd)
+#iMg$grp1_base_sd[missing.sd] = with(iMg, grp1_base_se[missing.sd] * 
+#                                      sqrt(grp1_base_n[missing.sd]))
+iMg = iMg[!is.na(iMg$sd_2),] # remove 22 rows without sd
   
 groups = iMg %>%
   group_by(group) %>%
   mutate(n = sum(grp1_base_n)) %>%
   mutate(weighted.mean = grp1_base_mean * grp1_base_n / (n-1)) %>%
   mutate(mean = sum(weighted.mean)) %>%
-  mutate(q = (grp1_base_n - 1) * grp1_base_sd^2 + grp1_base_n * grp1_base_mean^2) %>%
+  mutate(q = (grp1_base_n - 1) * sd_2^2 + grp1_base_n * grp1_base_mean^2) %>%
   mutate(qc = sum(q)) %>%
   mutate(var = (qc - n * mean^2) / (n-1)) %>%
   mutate(sd = sqrt((qc - n * mean^2) / (n-1)))
@@ -31,16 +31,16 @@ unique(report1)
 ########## this code leave in the lines that only have mean until overall mean has been calculated
 ############ and then it removes those lines and recalculates n and calculates overall sd
 
-iMg = read_csv("iMg_EXTRACTION_Resultsfinal_Ansu.csv")
-iMg = iMg[,c(1, 4, 7, 9, 10, 11, 12)] # 261 rows
+iMg = read_csv("iMgReview.csv")
+iMg = iMg[,c(1, 4, 7, 9, 10, 12)] # 261 rows
 iMg = iMg[!is.na(iMg$grp1_base_mean),] # remove 18 rows without means
 iMg = iMg[!is.na(iMg$grp1_base_n),] # remove 10 rows without sample sizes
 iMg = unite(iMg, group, 2:3, remove = T)
 
 # replace missing sd with sd calculated from se and n
-missing.sd = is.na(iMg$grp1_base_sd)
-iMg$grp1_base_sd[missing.sd] = with(iMg, grp1_base_se[missing.sd] * 
-                                      sqrt(grp1_base_n[missing.sd]))
+#missing.sd = is.na(iMg$sd_2)
+#iMg$sd_2[missing.sd] = with(iMg, grp1_base_se[missing.sd] * 
+#                                      sqrt(grp1_base_n[missing.sd]))
 
 
 groups = iMg %>%
@@ -49,11 +49,11 @@ groups = iMg %>%
   mutate(weighted.mean = grp1_base_mean * grp1_base_n / (n-1)) %>%
   mutate(mean = sum(weighted.mean))
 
-groups = groups[!is.na(groups$grp1_base_sd),] # remove 22 rows without sd
+groups = groups[!is.na(groups$sd_2),] # remove 22 rows without sd
 
 groups = groups %>%
   mutate(new_n = sum(grp1_base_n)) %>%
-  mutate(q = (grp1_base_n - 1) * grp1_base_sd^2 + grp1_base_n * grp1_base_mean^2) %>%
+  mutate(q = (grp1_base_n - 1) * sd_2^2 + grp1_base_n * grp1_base_mean^2) %>%
   mutate(qc = sum(q)) %>%
   mutate(var = (qc - new_n * mean^2) / (n-1)) %>%
   mutate(sd = sqrt((qc - new_n * mean^2) / (n-1)))
@@ -80,8 +80,8 @@ m2 = m2 %>%
   mutate(lower = mean - 1.96 * sd) %>%
   mutate(upper = mean + 1.96 * sd)
 
-m1 = separate(m1, group, c("health condition", "metric"), sep = "_")
-m2 = separate(m2, group, c("health condition", "metric"), sep = "_")
+mm1 = separate(m1, group, c("health condition", "metric"), sep = "_")
+mm2 = separate(m2, group, c("health condition", "metric"), sep = "_")
 
 m1[6,2] = "Serum Mg"
 m2[6,2] = "Serum Mg"
